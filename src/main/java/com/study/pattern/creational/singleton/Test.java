10a20d50a02962826ef6099561db39bef6da0df7
pgs <845486067@qq.com>
2018-10-25 07:15:40 +0800

    增加测试-序列化破坏单例

--- src/main/java/com/study/pattern/creational/singleton/Test.java
+++ src/main/java/com/study/pattern/creational/singleton/Test.java
@@ -1,16 +1,29 @@
 package com.study.pattern.creational.singleton;
 
+import java.io.*;
+
 /**
  * @author 潘根山
  * @create 2018-10-11 21:52
  * @since 1.0.0
  */
 public class Test {
-    public static void main(String[] args) {
-        Thread t1 = new Thread(new T());
-        Thread t2 = new Thread(new T());
-        t1.start();
-        t2.start();
-        System.out.println("Program end");
+    //    public static void main(String[] args) {
+//        Thread t1 = new Thread(new T());
+//        Thread t2 = new Thread(new T());
+//        t1.start();
+//        t2.start();
+//        System.out.println("Program end");
+//    }
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String file = "singleton_file";
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File(file)));
+        HungrySingleton instance = HungrySingleton.getInstance();
+        oos.writeObject(instance);
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(file)));
+        HungrySingleton newInstance = (HungrySingleton) ois.readObject();
+        System.out.println(instance);
+        System.out.println(newInstance);
+        System.out.println(instance == newInstance);
     }
 }
